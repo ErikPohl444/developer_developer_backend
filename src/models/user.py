9fafd437067a17952f7d3fcf2b8740012f5dfce1
Erik Pohl <erik.pohl.444@gmail.com>
2025-06-22 16:54:58 -0400

    moved user class into user module in models and user endpoints into user service under services

---
+++ src/models/user.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    user_id: int | None = Field(default=None, primary_key=True)
+    name: str
+    age: int
+    email: str
