99b7300d415fdd5fd9f0b246631348410b97558d
Erik Pohl <erik.pohl.444@gmail.com>
2025-06-26 13:48:05 -0400

    made sure to reference foreign keys in the models when needed

--- src/models/persongoal.py
+++ src/models/persongoal.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, Field
 
 class PersonGoal(SQLModel, table=True):
     persongoal_id: int | None = Field(default=None, primary_key=True)
-    goal_id: int
-    person_id: int
+    goal_id: int = Field(foreign_key="goal.goal_id")
+    person_id: int = Field(foreign_key="person.person_id")
     exceeds_score: int
     due_date: datetime.date
--- src/models/team.py
+++ src/models/team.py
@@ -3,6 +3,6 @@ from sqlmodel import SQLModel, Field
 
 class Team(SQLModel, table=True):
     team_id: int | None = Field(default=None, primary_key=True)
-    parent_team_id: int
+    parent_team_id: int = Field(foreign_key="team.team_id")
     name: str
     manager: int
