cfb470505402e71d1ffdfedffd8132a688e6f00e
Erik Pohl <erik.pohl.444@gmail.com>
2025-06-26 14:26:34 -0400

    added a needed fk to skilltool

--- src/models/skilltool.py
+++ src/models/skilltool.py
@@ -9,4 +9,4 @@ class SkillTool(SQLModel, table=True):
     update_date: datetime.datetime = Field(default = datetime.date.today)
     skilltool_id: int | None = Field(default=None, primary_key=True)
     tool_id: int = Field(foreign_key="tool.tool_id")
-    skill_id: int
+    skill_id: int = Field(foreign_key="skill.skill_id")
